d33d1590ad697235b97e3e68c08b256f17535395
Tesoem <Tesoem@ISC-Tesoem-iMac.local>
2019-03-19 13:28:52 -0600

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/itics/daniel/barradezplasaminto/PrincipalActivity.java
@@ -0,0 +1,14 @@
+package mx.edu.tesoem.itics.daniel.barradezplasaminto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
+
